9cf532d78e9c25cb85d9f462293b237d52bf4194
Pit99 <p.cometti@studenti.unibs.it>
2019-04-30 11:26:56 +0200

    *ce*

---
+++ TamaGolem/src/it/unibs/fp/TamaGolem/Equilibrio.java
@@ -0,0 +1,12 @@
+package it.unibs.fp.TamaGolem;
+
+public class Equilibrio {
+	enum gemme  {
+		ACQUA,
+		FUOCO, 
+		ARIA 
+	};
+	
+	
+	
+}
---
+++ TamaGolem/src/it/unibs/fp/TamaGolem/Gemma.java
@@ -0,0 +1,21 @@
+package it.unibs.fp.TamaGolem;
+
+public class Gemma {
+	
+	private int tipo;
+
+	public Gemma(int tipo) {
+		super();
+		this.tipo = tipo;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+}
